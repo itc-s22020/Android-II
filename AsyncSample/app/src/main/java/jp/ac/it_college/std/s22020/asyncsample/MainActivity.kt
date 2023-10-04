c4ca6474c19ea8271f0f697fc468287e61f85378
itc-s22020 <s22020@std.it-college.ac.jp>
2023-10-04 12:18:14 +0900

    AsyncSample 11.4.7　途中

--- AsyncSample/app/src/main/java/jp/ac/it_college/std/s22020/asyncsample/MainActivity.kt
+++ AsyncSample/app/src/main/java/jp/ac/it_college/std/s22020/asyncsample/MainActivity.kt
@@ -2,10 +2,14 @@ package jp.ac.it_college.std.s22020.asyncsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s22020.asyncsample.databinding.ActivityMainBinding
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 
@@ -34,16 +38,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     @UiThread
-    private fun receivWeatherInfo(q : String) {
+    private fun receivWeatherInfo(q: String) {
         val url = "$WEATHER_INFO_URL&q&appid=$API_ID"
         val executorService = Executors.newSingleThreadExecutor()
-        val backgroundReceiver = WeatherInfoBackgroundReceiver()
+        val backgroundReceiver = WeatherInfoBackgroundReceiver(url)
         val future = executorService.submit(backgroundReceiver)
         val result = future.get()
     }
-    private class WeatherInfoBackgroundReceiver() : Callable<String> {
+
+    private class WeatherInfoBackgroundReceiver(val urlString: String) : Callable<String> {
         @WorkerThread
-        override fun call() : String {
+        override fun call(): String {
+            val url = URL(urlString)
+            val con = url.openConnection() as HttpURLConnection
+            con.apply {
+                connectTimeout = 1000
+                readTimeout = 1000
+                requestMethod = "GET"
+            }
+            try {
+                con.connect()
+                val result = con.inputStream.reader().readText()
+            }  catch (ex: SocketTimeoutException) {
+                Log.w(DEBUG_TAG, "通信タイムアウト", ex)
+            }
             return "hoge"
         }
     }
