3e56c7db5ea491d037e0286cbf7856da4edcfee5
itc-s22020 <s22020@std.it-college.ac.jp>
2023-10-17 10:41:48 +0900

    Media3ServiceSample MediaPlaybackService

---
+++ Media3ServiceSample/app/src/main/java/jp/ac/it_college/std/s22020/media3servicesample/MediaPlaybackService.kt
@@ -0,0 +1,40 @@
+package jp.ac.it_college.std.s22020.media3servicesample
+
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+class MediaPlaybackService : MediaSessionService() {
+    private var mediaSession: MediaSession? = null
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
+
+    private val playerListener = object : Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            when (playbackState) {
+                Player.STATE_ENDED -> this@MediaPlaybackService.stopSelf()
+                Player.STATE_BUFFERING -> {}
+                Player.STATE_IDLE -> {}
+                Player.STATE_READY -> {}
+
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val player = ExoPlayer.Builder(this).build()
+        player.addListener(playerListener)
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+        }
+        mediaSession = null
+        super.onDestroy()
+    }
+}
