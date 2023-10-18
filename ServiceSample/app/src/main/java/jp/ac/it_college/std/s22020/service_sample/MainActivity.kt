03cb90973916730f93cad9e33e4cbe3d54bb9a68
itc-s22020 <s22020@std.it-college.ac.jp>
2023-10-18 11:07:18 +0900

    ServiceSample cpl

--- ServiceSample/app/src/main/java/jp/ac/it_college/std/s22020/service_sample/MainActivity.kt
+++ ServiceSample/app/src/main/java/jp/ac/it_college/std/s22020/service_sample/MainActivity.kt
@@ -1,12 +1,29 @@
 package jp.ac.it_college.std.s22020.service_sample
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import jp.ac.it_college.std.s22020.service_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Log.i("SERVICE_SAMPLE", "権限がもらえた")
+                startSoundManagerService()
+            } else {
+                Log.i("SERVICE_SAMPLE", "権限がもらえなかった")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +34,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startSoundManagerService() {
+        if (!notificationPermissionRequest()) return
+
         val intent = Intent(this, SoundManageService::class.java)
         startService(intent)
         binding.btPlay.isEnabled = false
@@ -28,5 +47,27 @@ class MainActivity : AppCompatActivity() {
         stopService(intent)
         binding.btPlay.isEnabled = true
         binding.btStop.isEnabled = false
+    }
+
+    private fun notificationPermissionRequest(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return true
+
+        return when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                true
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                false
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+                false
+            }
+        }
     }
 }
