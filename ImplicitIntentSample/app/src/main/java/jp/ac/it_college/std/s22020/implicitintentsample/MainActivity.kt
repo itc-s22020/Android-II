c6ed120e1f6e044965bb48623e911932d03889d1
itc-s22020 <s22020@std.it-college.ac.jp>
2023-10-19 09:49:21 +0900

    ImplicitIntentSample 検索機能まで

--- ImplicitIntentSample/app/src/main/java/jp/ac/it_college/std/s22020/implicitintentsample/MainActivity.kt
+++ ImplicitIntentSample/app/src/main/java/jp/ac/it_college/std/s22020/implicitintentsample/MainActivity.kt
@@ -1,11 +1,30 @@
 package jp.ac.it_college.std.s22020.implicitintentsample
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import jp.ac.it_college.std.s22020.implicitintentsample.databinding.ActivityMainBinding
+import java.net.URI
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btMapSearch.setOnClickListener(::opMapSearchButton)
+    }
+
+    private fun opMapSearchButton(view: View) {
+        val searchWord = URLEncoder.encode(
+            binding.eaSearchWord.text.toString(), "UTF-8"
+        )
+        val uri = Uri.parse("geo:0,0?q=${searchWord}")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
     }
 }
